67ea62d69c16a545fdfd84874ff02414bb2a2afb
Eduardo <Eduardo@MacBook-Pro-de-Eduardo.local>
2015-04-10 18:35:59 -0300

    versao 0.95

--- app/admin/dm_clinica.rb
+++ app/admin/dm_clinica.rb
@@ -1,21 +1,10 @@
-ActiveAdmin.register DmClinica do
-
-
-  # See permitted parameters documentation:
-  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
-  #
-  # permit_params :list, :of, :attributes, :on, :model
-  #
-  # or
-  #
-  # permit_params do
-  #   permitted = [:permitted, :attributes]
-  #   permitted << :other if resource.something?
-  #   permitted
-  # end
-
+# -*- coding: utf-8 -*-
+ActiveAdmin.register DmClinica, namespace: :admin do
+  ESTADOS_BR = ['RJ','SP','DF','MG','PR']
+  config.batch_actions = false
+  menu parent: 'Base', label: 'Clinicas'
   filter :nome
-  
+
   index do
     column :dm_especialidades
     column :nome
@@ -26,6 +15,20 @@ ActiveAdmin.register DmClinica do
     column :terminio_atendimento
     column :ativo
     default_actions
+  end
+
+  form do |f|
+    f.inputs "Detalhes da clinica" do
+      f.input :nome
+      f.input :dm_especialidades, label: 'Especialidade', collection: DmEspecialidade.all.sort_by(&:especialidade), prompt: 'Selecionar'
+      f.input :cidade
+      f.input :dm_especialidades, label: 'Estado', collection: ESTADOS_BR, prompt: 'Selecionar'
+      f.input :telefone
+      f.input :inicio_atendimento
+      f.input :terminio_atendimento
+      f.input :ativo   
+    end
+    f.actions
   end
 
 end
